3ae7e42e1a860b233c20506934c2338e18d59119
Kelvin Tiongson <kelvintiongson30@gmail.com>
2018-01-08 18:13:49 -0600

    fixed jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,22 @@
 // set up MySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: "root",
-	database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+// 	port: 3306,
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "root",
+// 	database: "burgers_db"
+// });
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
-		host: 'l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		user: '	qedtdge0lmkturoe',
-		password: 'i9sg2xyr7ygdjuxo',
-		database: 'yd8iruiuo8kjld26'
+		host: 'localhost',
+		user: '	root',
+		password: 'root',
+		database: 'burgers_db'
 	});
 };
 
